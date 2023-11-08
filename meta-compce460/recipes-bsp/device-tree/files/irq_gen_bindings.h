0bc80aaad63c5362f897c72bac3d6dee865c8e44
Andreas Stergiopoulos <andreas.stergiopoulos@tuni.fi>
2023-11-08 21:25:43 +0200

    Reverted changes in bindings

--- meta-compce460/recipes-bsp/device-tree/files/irq_gen_bindings.h
+++ meta-compce460/recipes-bsp/device-tree/files/irq_gen_bindings.h
@@ -3,19 +3,20 @@
  */
 
 // ACK values for level type interrupt handling per IRQ line
-#define ACK1d 0x1d
-#define ACK1e 0x1e
-#define ACK1f 0x1f
-#define ACK20 0x20
-#define ACK21 0x21
-#define ACK22 0x22
-#define ACK23 0x23
-#define ACK24 0x24
-#define ACK34 0x34
-#define ACK35 0x35
-#define ACK36 0x36
-#define ACK37 0x37
-#define ACK38 0x38
-#define ACK39 0x39
-#define ACK3a 0x3a
-#define ACK3b 0x3b
+#define ACK1d 0x0
+#define ACK1e 0x1
+#define ACK1f 0x2
+#define ACK20 0x3
+#define ACK21 0x4
+#define ACK22 0x5
+#define ACK23 0x6
+#define ACK24 0x7
+#define ACK34 0x8
+#define ACK35 0x9
+#define ACK36 0xa
+#define ACK37 0xb
+#define ACK38 0xc
+#define ACK39 0xd
+#define ACK3a 0xe
+#define ACK3b 0xf
+
